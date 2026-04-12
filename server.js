15551e8c8396f6fd4165ddb8200009c2b6e331bf
xiaohei123356 <3354397097@qq.com>
2026-04-12 21:07:08 +0800

    fix: 真正修改 server 绑定地址为 0.0.0.0

--- server.js
+++ server.js
@@ -94,8 +94,8 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
 
 function loadEnvFile() {
